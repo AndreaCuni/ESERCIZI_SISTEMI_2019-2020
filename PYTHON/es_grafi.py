cf5eaff455c92d33aebe3486f4ac288ee8e381b9
AndreaCuni <48799873+AndreaCuni@users.noreply.github.com>
2020-02-27 10:43:16 +0100

    Update es_grafi.py

--- PYTHON/es_grafi.py
+++ PYTHON/es_grafi.py
@@ -23,11 +23,24 @@ def adj2Dict(m):
 def dict2Adj(d):
     matrix = []
 
-    for _, n in d.items():      #_, si utilizza per ignorare la prima parte d
+    for _, n in d.items():      #_, si utilizza per ignorare la prima parte di ogni elemento, quindi la chiave
         matrix.append(n)
     print(matrix)
 
 #fare anche per non orientati pesati
+def user2DictPes():
+    nodi = int(input("inserisci il numero di nodi"))
+    m = []
+
+    for i in range(0,nodi):
+        arco = [int(n) for n in input(f"a quali altri nodi è collegato il nodo {i}:").split(",")]
+        colonne = [0 for n in range (0,nodi)]
+        for k in arco:
+            if (k != -1):
+                colonne.append(1)
+        m.append(colonne)
+    return m  
+
 #fare disegno del grafo con networkX
 
 
