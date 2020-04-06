31d79b6937cbb9642e7f74d373d10fd11fa574f0
AndreaCuni <48799873+AndreaCuni@users.noreply.github.com>
2020-04-06 21:49:17 +0200

    esercizi vari

--- PYTHON/prova_dizionari.py
+++ PYTHON/prova_dizionari.py
@@ -1,7 +1,25 @@
 d = {1:[1,2],2:[2,4]}
 
-matrix = []
-for _, n in d.items():
-    matrix.append(n)
+grid = [[True, True, True, True, True, False],
+    [False, False, True, True, True, False],
+    [True, True, True, False, True, True],
+    [True, False, False, True, True, False],
+    [True, False, True, True, False, True],
+    [True, False, True, True, True, False]]
 
-print(matrix)
+
+n = 1
+defM = []
+matrix = []
+
+for i in grid:
+    matrix = []
+    for x in i:
+        if (x == True):
+            matrix.append(n)
+            n=n+1
+        else: 
+            matrix.append(-1) 
+    defM.append(matrix)
+
+print (defM)
